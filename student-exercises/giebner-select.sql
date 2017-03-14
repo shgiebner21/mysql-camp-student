3332df2cf75c9a30ee8016e0259d7e91d283061f
Stephen Giebner <shgiebner21@gmail.com>
2017-03-14 13:40:29 -0400

    SQL wildcards

--- student-exercises/giebner-select.sql
+++ student-exercises/giebner-select.sql
@@ -21,3 +21,15 @@ SELECT ID, lastname, birthDate FROM individual WHERE Year(birthDate) > 1940;
 SELECT 	name, genre, isTogether FROM band WHERE  genre = 'alternative' AND isTogether = 0;
 
 SELECT id, name FROM band WHERE id = 4;
+
+# SQL Wildcards
+
+SELECT * FROM band WHERE name LIKE 	'The%';
+SELECT * FROM band WHERE name LIKE '%Stones';
+SELECT * FROM individual WHERE firstName LIKE '__ck';
+
+SELECT * FROM band WHERE name LIKE '% and %';
+
+SELECT * FROM individual WHERE firstName LIKE '___';
+
+SELECT * FROM band WHERE name LIKE '%s';
