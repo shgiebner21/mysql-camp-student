d7d5bd4197ff5ab0399c26e96f38755dbca6a031
Stephen Giebner <shgiebner21@gmail.com>
2017-03-14 11:49:43 -0400

    restricting rows returned

--- student-exercises/giebner-select.sql
+++ student-exercises/giebner-select.sql
@@ -1,5 +1,19 @@
-USE rockStarDay2;
+ USE rockStarDay2;
 
 SHOW COLUMNS FROM band;
 
+SELECT * FROM band;
+
 SELECT ID, name, genre FROM band;
+
+SELECT ID, name, genre
+FROM band
+WHERE isTogether= '1';
+
+SELECT * FROM individual WHERE lastName= 'Jennings';
+
+SELECT ID, lastName From individual WHERE deceasedDate is NOT NULL;
+
+SELECT ID, lastname, birthDate FROM individual WHERE Year(birthDate) > 1940;
+
+SELECT * FROM individual WHERE ID IN (1, 3, 5, 7, 19);
